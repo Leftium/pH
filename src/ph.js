817af39ed2e84e3c25c2d6b551f00f6faf6e3365
Leftium <john@leftium.com>
2016-12-03 13:31:59 -0600

    Refactor to work smoothly on mobile.

--- src/ph.js
+++ src/ph.js
@@ -1,21 +1,73 @@
-pw = debounce(function() {
-    GenerateToTextField();
-    var resultField = document.getElementById('hashedPassword');
-    resultField.style.visibility = 'visible';
-    resultField.focus();
-    if (document.selection) {
-        var sel = document.selection.createRange();
-        sel.move(0, resultField.value.length);
-        sel.select();
+// From: https://github.com/zenorocha/select/blob/master/src/select.js
+function select(element) {
+    var selectedText;
+
+    if (element.nodeName === 'SELECT') {
+        element.focus();
+
+        selectedText = element.value;
+    }
+    else if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
+        element.focus();
+        element.setSelectionRange(0, element.value.length);
+
+        selectedText = element.value;
     }
     else {
-        resultField.selectionStart = 0;
-        resultField.selectionEnd = resultField.value.length;
+        if (element.hasAttribute('contenteditable')) {
+            element.focus();
+        }
+
+        var selection = window.getSelection();
+        var range = document.createRange();
+
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        selectedText = selection.toString();
     }
-    document.execCommand('copy');
-    document.getElementById('sitePassword').focus();
+
+    return selectedText;
+}
+
+// Based on:
+// https://www.reddit.com/r/web_design/comments/33kxgf/javascript_copying_to_clipboard_is_easier_than/
+// https://github.com/zenorocha/clipboard.js/blob/294e9fcb5d80c3966d18bae635cbcccfe9cd8664/src/clipboard-action.js
+
+function copytext(text) {
+    var success = false;
+    var sitePasswordField = document.getElementById('sitePassword');
+    var savedValue = sitePasswordField.value;
+
+    sitePasswordField.value = text;
+
+    sitePasswordField.type = 'text';
+    window.selectedText = select(sitePasswordField);
+    success = document.execCommand('copy');
+    sitePasswordField.type = 'password'
+
+    sitePasswordField.value = savedValue;
+    console.log(sitePasswordField);
+    return success;
+}
+
+pw = function() {
+    GenerateToTextField();
+    var sitePasswordField = document.getElementById('sitePassword');
+    var resultField = document.getElementById('hashedPassword');
+
+    var success = copytext(resultField.value);
+    if (success) {
+        sitePasswordField.style.backgroundColor = 'lightgreen';
+    } else {
+        sitePasswordField.style.backgroundColor = 'pink';
+    }
+
+    console.log(success, window.selectedText);
     document.getElementById('result').className = 'generated';
-}, 500);
+};
+
 var extractDomain = new SPH_DomainExtractor().extractDomain
 if (!window.location.origin) {
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
@@ -25,6 +77,9 @@ function init() {
     bookmarkletScript = bookmarkletScript.replace(/ORIGIN/, location.origin);
     var bookmarklet = document.getElementById('bookmarklet');
     bookmarklet.href = bookmarkletScript;
+
+    sitePasswordField = document.getElementById('sitePassword');
+    sitePasswordField.onkeyup = pw;
 
     var domainField = document.getElementById('domain');
     if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
@@ -50,27 +105,3 @@ function domainToLower() {
     var domainField = document.getElementById('domain');
     domainField.value = domainField.value.toLowerCase();
 }
-// From: https://davidwalsh.name/function-debounce
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-    var timeout;
-    var last = new Date();
-    return function() {
-        var now = new Date();
-        console.log((now - last)/1000);
-        last = now;
-
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
