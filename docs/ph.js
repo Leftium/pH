7e2a1e5ee3ad39d4b7bf2c3385b2f2bb4a3b9f0c
Leftium <john@leftium.com>
2016-12-03 04:06:37 -0600

    Update GH pages.

--- docs/ph.js
+++ docs/ph.js
@@ -1,21 +1,44 @@
-pw = debounce(function() {
+var textField = document.createElement('textarea');
+function copytext(text) {
+    textField.innerText = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    document.body.removeChild(textField);
+}
+
+pw = function() {
+    // console.log(arguments);
+    sitePasswordField = document.getElementById('sitePassword');
+    sitePasswordField.style.backgroundColor = 'white';
+    e = arguments[0];
     GenerateToTextField();
     var resultField = document.getElementById('hashedPassword');
     resultField.style.visibility = 'visible';
     resultField.focus();
-    if (document.selection) {
-        var sel = document.selection.createRange();
-        sel.move(0, resultField.value.length);
-        sel.select();
+
+    copytext(resultField.value);
+
+    var success
+    try {
+        success = document.execCommand('copy');
+    } catch (error) {
+        console.log(error);
     }
-    else {
-        resultField.selectionStart = 0;
-        resultField.selectionEnd = resultField.value.length;
+
+    if (success) {
+        sitePasswordField.style.backgroundColor = 'lightgreen';
+    } else {
+        sitePasswordField.style.backgroundColor = 'pink';
     }
-    document.execCommand('copy');
+
+
+    console.log(success, e.isTrusted, sitePasswordField.style.backgroundColor);
+
     document.getElementById('sitePassword').focus();
     document.getElementById('result').className = 'generated';
-}, 500);
+};
+
 var extractDomain = new SPH_DomainExtractor().extractDomain
 if (!window.location.origin) {
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
@@ -25,6 +48,9 @@ function init() {
     bookmarkletScript = bookmarkletScript.replace(/ORIGIN/, location.origin);
     var bookmarklet = document.getElementById('bookmarklet');
     bookmarklet.href = bookmarkletScript;
+
+    sitePasswordField = document.getElementById('sitePassword');
+    sitePasswordField.oninput = pw;
 
     var domainField = document.getElementById('domain');
     if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
@@ -50,27 +76,3 @@ function domainToLower() {
     var domainField = document.getElementById('domain');
     domainField.value = domainField.value.toLowerCase();
 }
-// From: https://davidwalsh.name/function-debounce
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-    var timeout;
-    var last = new Date();
-    return function() {
-        var now = new Date();
-        console.log((now - last)/1000);
-        last = now;
-
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
