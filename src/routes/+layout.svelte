ce841c7c4fb4cb01e1eed0919b57f4d5623698f2
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-09-07 13:35:25 +0900

    feat: apply logo kit favicon and web manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import '@leftium/nimble.css/core';
-	import favicon from '#lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
